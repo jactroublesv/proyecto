273cb8e0baf8d33199db1c87415de141830cf124
jactrouble <jactrouble@192.168.1.8>
2020-06-08 03:20:37 -0600

    En uso

---
+++ src/main/java/modelo/sale.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author jactrouble
+ */
+public class sale {
+    
+    private int id;
+    private String info;
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the info
+     */
+    public String getInfo() {
+        return info;
+    }
+
+    /**
+     * @param info the info to set
+     */
+    public void setInfo(String info) {
+        this.info = info;
+    }
+    
+}
